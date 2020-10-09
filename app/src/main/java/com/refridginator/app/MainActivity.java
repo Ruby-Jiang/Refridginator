162e45772246e6874aab93b7a5f60fa64edba511
Stacy Wandrey <Stacy.Wandrey@ucdconnect.ie>
2020-10-09 17:30:45 +0100

    Changed the UI to display all of our feature ideas. Began adding the activities to display

--- app/src/main/java/com/refridginator/app/MainActivity.java
+++ app/src/main/java/com/refridginator/app/MainActivity.java
@@ -2,7 +2,10 @@ package com.refridginator.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView image = findViewById(R.id.addFood);
+        image.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AddAFood.class );
+
+            startActivity(intent);
+
+        });
     }
 }
---
+++ app/src/main/java/com/refridginator/app/StorageAndFood.java
@@ -0,0 +1,4 @@
+package com.refridginator.app;
+
+public class StorageAndFood {
+}
