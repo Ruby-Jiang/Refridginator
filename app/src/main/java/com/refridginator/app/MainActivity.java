8d62505c694a97020a3fbcab132af346d4cd57c1
Stacy Wandrey <Stacy.Wandrey@ucdconnect.ie>
2020-10-16 11:01:35 +0100

    Adds access to the recipe API but not displayed

--- app/src/main/java/com/refridginator/app/MainActivity.java
+++ app/src/main/java/com/refridginator/app/MainActivity.java
@@ -10,11 +10,40 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String URL="https://api.edamam.com/search?q=chicken&app_id=66e1ad0a&app_key=40b611f403c5665eb034a5bcafa94947&from=0&to=3&calories=591-722&health=alcohol-free";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest=new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.e("Rest Response", response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Rest Response", error.toString());
+
+            }
+        }
+        );
+        requestQueue.add(objectRequest);
 
         ImageView image = findViewById(R.id.addFood);
 //        image.setOnClickListener(v -> {
