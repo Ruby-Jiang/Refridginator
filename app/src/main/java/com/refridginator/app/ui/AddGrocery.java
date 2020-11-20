62fba75a796a61ff22bf1a15cd84b08f23031c79
lgs <leeguosheng@126.com>
2020-11-20 12:30:51 +0000

    addgrocery_Grocerylist_recyclerview

--- app/src/main/java/com/refridginator/app/ui/AddGrocery.java
+++ app/src/main/java/com/refridginator/app/ui/AddGrocery.java
@@ -1,8 +1,12 @@
 package com.refridginator.app.ui;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
@@ -18,95 +22,94 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.refridginator.app.R;
+import com.refridginator.app.data.Item;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class AddGrocery extends AppCompatActivity {
 
-    private Button btn_add;
-    private LinearLayout container;
+    ArrayList<Item> items;
+    private int itemQuantity;
+    //    private Item[] items = new Item[]{new Item("afasf",2)};
+    private static final String TAG = "GroceryListAct";
+    private GrolistAdapter glAdapter;
+    private LinearLayoutManager layoutManager;
+    private RecyclerView recyclerView;
 
+    public void get_set_ItemQuantity(){
+        itemQuantity=0;
+        for (int i=0; i< items.size(); i++){
+            itemQuantity += items.get(i).getNumber();
+        }
+        TextView quantity_tv = findViewById(R.id.textView4);
+        quantity_tv.setText(itemQuantity+"");
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_grocery);
-        btn_add = findViewById(R.id.btn_add);
-        container = findViewById(R.id.container);
-        alertinfo("Hey look you made it to the grocery list", 1000);
-        btn_add.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_recycle_view);
+
+        items = new ArrayList<>();
+//        items.add(new Item("apple", 2));
+//        items = new Item[]{new Item("sada", 9)};
+//        items.add(0, new Item("sada", 9));
+        recyclerView = this.findViewById(R.id.recyclerview);
+        layoutManager = new LinearLayoutManager(this);
+        glAdapter = new GrolistAdapter(this, items);
+//
+        recyclerView.setAdapter(glAdapter);
+        recyclerView.setLayoutManager(layoutManager);
+
+        View addItemButton = findViewById(R.id.btn_add_item);
+//        addItemButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(GroceryListAct.this, RecycleViewAct.class));
+//            }
+//        });
+        addItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (container instanceof ViewGroup) {
-                    ViewGroup vp = (ViewGroup) container;
-                    int index = vp.getChildCount();
-                    EditText current_edittext = null;
-                    //if index = 0. No elements in the container
-                    if (index != 0){
-                        //find the last EditText view
-                        for (int i=index-1; i>=0; i--){
-                            if (vp.getChildAt(i) instanceof EditText){
-                                current_edittext =(EditText) vp.getChildAt(i);
-                                break;
-                            }
-                        }
+                final EditText inputServer = new EditText(AddGrocery.this);
+                final AlertDialog.Builder builder = new AlertDialog.Builder(AddGrocery.this);
+                builder.setTitle("Write down your wish item").setIcon(android.R.drawable.ic_dialog_info).setView(inputServer)
+                        .setNegativeButton("Cancel", null);
+                recyclerView.removeView(inputServer);
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        int fillPosition = items.size();
+                        String inout = inputServer.getText().toString();
+                        items.add(fillPosition, new Item(inout, 1));
+                        glAdapter.notifyItemInserted(fillPosition);
+                        get_set_ItemQuantity();
                     }
-                    //add a new edittext view into the container
-                    addView(current_edittext, index);
-                }
+                });
+                builder.show();
             }
         });
-    }
 
-    protected void addView(EditText current_edittext, int index){
-        //If curent edittext's text is empty, alert the user to fill in rather than create a new edittext view
-        if (current_edittext != null) {
-            if (TextUtils.isEmpty(current_edittext.getText())){
-                alertinfo("Please fill in the item", 1000);
-                return;
-            }
-        }
-        EditText child = new EditText(this);
-        child.setFocusable(true);
-        child.setFocusableInTouchMode(true);
-        child.requestFocus();
-//        child.setBackgroundColor(Color.parseColor("#00ffff"));
-        child.setTextSize(25);
-        child.setPadding(10,0,0,10);
-        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) current_edittext.getLayoutParams();
-        params.setMargins(10,5,5,10);
-        child.setLayoutParams(params);
-        GradientDrawable drawable = new GradientDrawable();
-        drawable.setCornerRadius(20);
-        drawable.setColor(Color.parseColor("#00ffff"));
-        child.setBackground(drawable);
-//        child.setWidth();
-        child.setSingleLine(true);
-//        child.setTextSize(20);
-//        child.setTextColor(getResources().getColor(R.color.colorAccent));
-        // 获取当前的时间并转换为时间戳格式, 并设置给TextView
-//        String currentTime = dateToStamp(System.currentTimeMillis());
-        child.setHint("Please add an item");
-//        child.setText(currentTime);
-        // 调用一个参数的addView方法
-        container.addView(child, index);
-    }
-
-    //A function to alert the users some infos
-    protected void alertinfo(String alert_msg, int timedelay){
-        final AlertDialog alert = new AlertDialog.Builder(AddGrocery.this)
-                .create();
-        Timer timer = new Timer();
-        //make alert info dismiss
-        TimerTask alert_to_fill = new TimerTask() {
+        glAdapter.setBtnDelListener(new GrolistAdapter.BtnOnClickListerner() {
             @Override
-            public void run() { alert.dismiss(); }
-        };
-        alert.setMessage(alert_msg);
-        alert.show();
-        timer.schedule(alert_to_fill, timedelay);
+            public void onDeleteClick(int postion) {
+                items.remove(postion);
+                glAdapter.notifyItemRangeRemoved(postion, 1);
+                get_set_ItemQuantity();
+            }
+
+            @Override
+            public void onAddQuantityClick(int position) {
+//                Toast.makeText(GroceryListAct.this, position+"", Toast.LENGTH_SHORT).show();
+                Item e = items.get(position);
+                e.setName(e.getName());
+                e.setNumber(e.getNumber()+1);
+                glAdapter.notifyItemChanged(position);
+                get_set_ItemQuantity();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/refridginator/app/ui/GrolistAdapter.java
@@ -0,0 +1,143 @@
+package com.refridginator.app.ui;
+
+//import candroid.content.Context;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.refridginator.app.R;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.refridginator.app.data.Item;
+
+import java.util.ArrayList;
+
+public class GrolistAdapter extends RecyclerView.Adapter<GrolistAdapter.ItemHolder>{
+    private ArrayList<Item> items;
+//    private Item[] items;
+//    private int delimgsrc = R.drawable.trash;
+//    private int addquantitysrc = R.drawable.plus1;
+    private Context context;
+    private BtnOnClickListerner btnDelListener;
+
+    public void setBtnDelListener(BtnOnClickListerner btnDelListener) {
+        this.btnDelListener = btnDelListener;
+    }
+
+    public interface getItem{
+        String getItemIput(String iteminput);
+    }
+
+    public interface BtnOnClickListerner{
+        void onDeleteClick(int postion);
+        void onAddQuantityClick(int position);
+    }
+    public GrolistAdapter(Context context, ArrayList<Item> items) {
+        this.items = items;
+        this.context = context;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    @NonNull
+    @Override
+    public GrolistAdapter.ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item_row, parent, false);
+        return new ItemHolder(v, btnDelListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ItemHolder holder, final int position){
+        if (this.items.get(position) == null){return;}
+        holder.itename.setText(this.items.get(position).getName());
+        holder.quantity.setText(this.items.get(position).getNumber()+"");
+        holder.deletebutton.setImageResource(R.drawable.trash);
+        holder.addquantity.setImageResource(R.drawable.plus1);
+        holder.x.setText("X");
+
+        holder.itename.setFocusable(true);
+        holder.itename.requestFocus();
+        holder.itename.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String ItemIput = s.toString();
+//                int index = holder.getAdapterPosition();
+////                if (items.get(index) == null){
+////                    items.set(index, new Item(ItemIput,1));
+////                    return;
+////                }
+//                Toast.makeText(context, index+"", Toast.LENGTH_LONG).show();
+//                items.add(index, new Item(ItemIput, Integer.parseInt((String) holder.quantity.getText())));
+//                items.set(holder.getAdapterPosition(), ItemIput);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String ItemIput = s.toString();
+//                int index = holder.getAdapterPosition();
+////                if (items.get(index) == null){
+////                    items.set(index, new Item(ItemIput,1));
+////                    return;
+////                }
+//                Toast.makeText(context, index+"", Toast.LENGTH_LONG).show();
+//                items.add(index, new Item(ItemIput, Integer.parseInt((String) holder.quantity.getText())));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public static class ItemHolder extends RecyclerView.ViewHolder{
+        private static final String TAG = "ItemHolder";
+        TextView itename;
+        TextView quantity;
+        ImageView deletebutton;
+        ImageView addquantity;
+        TextView x;
+
+        public ItemHolder(@NonNull View itemView, final BtnOnClickListerner listerner) {
+            super(itemView);
+            Log.d(TAG, "MyViewHolder: called");
+            this.itename = itemView.findViewById(R.id.tv_itemname);
+            this.quantity = itemView.findViewById(R.id.tv_quantity);
+            this.deletebutton = itemView.findViewById(R.id.delete_button);
+            this.addquantity = itemView.findViewById(R.id.btn_add_quantity);
+            this.x = itemView.findViewById(R.id.tv_x);
+
+            deletebutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    listerner.onDeleteClick(position);
+                }
+            });
+
+            addquantity.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    listerner.onAddQuantityClick(position);
+                }
+            });
+        }
+    }
+
+}
